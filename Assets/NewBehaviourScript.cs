27d77e09b51cae1d1479f1bbdb88d75a0d048f80
zhouy546 <zhouyunqi546@gmail.com>
2018-04-24 22:30:52 +0800

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+    [SerializeField]
+    Text text;
+
+    string stext;
+    char[] theChar;
+    void Start() {
+        stext = text.text;
+        theChar = stext.ToCharArray();
+        PunctuationCheck(theChar);
+    }
+
+    void PunctuationCheck(char[] _char)
+    {
+        for (int i = 0; i < _char.Length; i++)
+        {
+            if (Char.IsPunctuation(_char[i])) {
+                Debug.Log(_char[i].ToString());
+            }
+        }
+
+    }
+
+}
